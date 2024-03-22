2e2cca23159c7efa9a6954f1b6f63ba911e5952a
centinna-l <jerryjoy99@gmail.com>
2024-03-22 10:58:09 -0400

    UI Fixes

--- app/src/main/java/com/example/quick/StartActivity.java
+++ app/src/main/java/com/example/quick/StartActivity.java
@@ -1,6 +1,7 @@
 package com.example.quick;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -35,6 +36,7 @@ public class StartActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         setContentView(R.layout.activity_start);
 
         iconImage = findViewById(R.id.icon_image);
